a40931863ddddefa584ccdf34bc854e15e078cf7
G-Robot <takahiro884@gmail.com>
2026-02-26 18:41:51 +0900

    Add files via upload

---
+++ inazawa-sw.js
@@ -0,0 +1,29 @@
+var CACHE_NAME = 'inazawa-cache-v1';
+
+self.addEventListener('install', function(event) {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(name) {
+          return name !== CACHE_NAME;
+        }).map(function(name) {
+          return caches.delete(name);
+        })
+      );
+    }).then(function() {
+      return self.clients.claim();
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
